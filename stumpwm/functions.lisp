9a7705568263d341bd0c11d98f3280612a672714
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-14 14:28:26 +0100

    reindent

--- stumpwm/functions.lisp
+++ stumpwm/functions.lisp
@@ -1,15 +1,17 @@
 ;; toggle hor/vert split (only with 2 windows)
 ;;rotate windows....
 (defun shift-windows-forward (frames win)
-"Exchange windows through cycling frames."
+  "Exchange windows through cycling frames."
   (when frames
-          (let ((frame (car frames)))
-                  (shift-windows-forward (cdr frames)
-                                         (frame-window frame))
-                  (when win
-                           (pull-window win frame)))))
+    (let ((frame (car frames)))
+      (shift-windows-forward (car frames)
+                             (frame-window frame))
+      (when win
+        (pull-window win frame)))))
 (defcommand rotate-windows () ()
   (let* ((frames (group-frames (current-group)))
-            (win (frame-window (car (last frames)))))
-          (shift-windows-forward frames win)))
+         (win (frame-window (car (last frames)))))
+    (shift-windows-forward frames win)))
+
 (define-key *top-map* (kbd "s-]") "rotate-windows")
+
