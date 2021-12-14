225507e0c74a23dc8dbd9f893e728cf480d949bd
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-14 23:17:43 +0100

    Add polybar config to show packages and gh notif

---
+++ polybar/checkgh.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+[[ ${1} == "open" ]] && {
+    xdg-open 'https://github.com/notifications?query=reason%3Aparticipating+is%3Aunread++'
+    exit
+}
+n=$(gh api -X GET /notifications --cache=1m -f per_page='30' \
+       -f participating='true' --jq '.|length')
+[[ ${n} > 0 ]] && echo " ${n}"
