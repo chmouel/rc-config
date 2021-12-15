e7366d8450b78da04482f63b58ba7cf760e645ab
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-15 09:57:36 +0100

    remove gaps and non used stuff
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- stumpwm/modules.lisp
+++ stumpwm/modules.lisp
@@ -1,11 +1,3 @@
-;;; Gaps
-(load-module "swm-gaps")
-; ;; Inner gaps run along all the 4 borders of a window
-(setf swm-gaps:*inner-gaps-size* 2)
-(when *initializing*
-  (swm-gaps:toggle-gaps))
-(define-key *groups-map* (kbd "g") "toggle-gaps")
-
 (load-module "globalwindows")
 (define-key *top-map* (kbd "s-`") "global-windowlist")
 (define-key *top-map* (kbd "s-~") "global-pull-windowlist")
@@ -13,9 +5,6 @@
 (load-module "clipboard-history")
 (define-key *root-map* (kbd "C-y") "show-clipboard-history")
 (clipboard-history:start-clipboard-manager)
-
-;; (load-module "swm-golden-ratio")
-;; (swm-golden-ratio:toggle-golden-ratio)
 
 (load-module "pass")
 
