485c5a531de7cd67d686d635dd5dc4dc78a1f0f4
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-16 14:45:24 +0100

    add locking if exist on stumpwm

---
+++ stumpwm/lock.lisp
@@ -0,0 +1,8 @@
+(when (and *initializing*
+           (and (probe-file "/usr/bin/xss-lock")
+                (probe-file "/usr/bin/i3lock")))
+  (setq lock-command "i3lock -c 000000 -f -e")
+  (define-key *top-map* (kbd "s-l") (concatenate 'string "run-shell-command " lock-command))
+  (run-shell-command "xset dpms 2000 2000 2000")
+  (run-shell-command "xset s 1800 30")
+  (run-shell-command (concatenate 'string "xss-lock -- " lock-command)))
