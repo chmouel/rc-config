98602e8615a873f0d84191ebd79a6afacb2e153e
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-19 11:26:09 +0100

    readd gap but only for big screens or hdmi connected

--- stumpwm/modules.lisp
+++ stumpwm/modules.lisp
@@ -5,4 +5,21 @@
 (define-key *root-map* (kbd "C-y") "show-clipboard-history")
 (clipboard-history:start-clipboard-manager)
 
+(load-module "swm-gaps")
+(setf swm-gaps:*inner-gaps-size* 10
+      swm-gaps:*outer-gaps-size* 7
+      swm-gaps:*head-gaps-size* 0)
+;; enable gap when we have a external monitor connect and not on laptop
+(when (and (getenv "MONITOR")
+           (string< "HDMI" (getenv "MONITOR"))
+           *initializing*)
+  (swm-gaps:toggle-gaps))
+(define-key *groups-map* (kbd "g") "toggle-gaps")
+
 (load-module "pass")
+
+;; vim: ft=lisp
+;; Local Variables:
+;; mode: lisp
+;; mode: stumpwm
+;; End:
