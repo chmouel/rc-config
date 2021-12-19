c9ea2a62f241c4054ff5ce02213a50734b06218c
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-19 22:08:52 +0100

    read -t at ze end!

--- polybar/scripts/arch-updates.sh
+++ polybar/scripts/arch-updates.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 [[ ${1} == yay ]] && {
     cd /tmp || exit
-    kitty-ctrl jump -t "Arch Updates" -- yay
+    kitty-ctrl jump -t "Arch Updates" /bin/sh -c "yay;read"
 }
 n=$(yay -Qu | wc -l)
 [[ ${n} -gt 0 ]] && echo "${n}"
