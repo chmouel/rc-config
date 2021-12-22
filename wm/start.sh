5bdbfaf7d064991e13cb3be9081ba9ba4d17be58
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-22 18:31:30 +0100

    Make it indepodent
    and add keycode 134 as Multi_key

--- wm/start.sh
+++ wm/start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euf
 exec 19>/tmp/.start.wmsession
 BASH_XTRACEFD=19
 set -x
@@ -6,38 +7,38 @@ set -x
 export WMSESSION="i3"
 
 autorandr --change
-
 setxkbmap -option "ctrl:nocaps"
-xinput set-prop 12 "libinput Tapping Enabled" 1
-xinput set-prop 12 "libinput Accel Speed" 0.5
-
+xmodmap -e "keycode 134 = Multi_key" -e "clear Lock"
+xinput set-prop 12 "libinput Tapping Enabled" 1 || true
+xinput set-prop 12 "libinput Accel Speed" 0.5 || true
 xsetroot -cursor_name left_ptr
 
 cd /tmp || exit
 
-[[ -n $(command -v picom) ]]  && nohup picom --no-fading-openclose  &
-[[ -n $(command -v nm-applet) ]]  && nohup nm-applet  &
-[[ -n $(command -v xsettingsd) ]] && nohup xsettingsd  &
-[[ -n $(command -v dunst) ]] && nohup dunst &
-[[ -x /usr/lib/gpaste/gpaste-daemon && -z $(pgrep gpaste-daemon) ]] && nohup /usr/lib/gpaste/gpaste-daemon &
-[[ -x /usr/lib/goa-daemon ]] && nohup /usr/lib/goa-daemon --replace &
-[[ -n $(command -v gnome-next-meeting-applet) ]] && nohup gnome-next-meeting-applet &
-
+[[ -n $(command -v picom) && -z $(pgrep -x picom) ]]  && nohup picom --no-fading-openclose  &
+[[ -n $(command -v nm-applet) && -z $(pgrep -x nm-applet)  ]]  && nohup nm-applet  &
+[[ -n $(command -v xsettingsd) && -z $(pgrep -x xsettingsd)  ]] && nohup xsettingsd  &
+[[ -n $(command -v dunst) && -z $(pgrep -x dunst) ]] && nohup dunst &
+[[ -x /usr/lib/gpaste/gpaste-daemon && -z $(pgrep -x gpaste-daemon) ]] && nohup /usr/lib/gpaste/gpaste-daemon &
+[[ -x /usr/lib/goa-daemon && -z $(pgrep -x goa-daemon) ]] && nohup /usr/lib/goa-daemon --replace &
+[[ -n $(command -v gnome-next-meeting-applet) && -z $(pgrep -f gnome-next-meeting) ]] && nohup gnome-next-meeting-applet &
 
 if [[ -n $(command -v xss-lock) && $(command -v i3lock) ]];then
     xset dpms 2000 2000 2000
     xset s 300 120
-    xss-lock -- i3lock -c 000000 -f -e &
+    [[ -z $(pgrep -x xss-lock) ]] && nohup xss-lock -- i3lock -c 000000 -f -e &
 fi
 cd $HOME || exit
 
 # since we are already on a gnome vibe, lets use gnome-keyring as our
 # ssh-agent, as a bonus it will give to sensible applications access to ssh
 # keys, password lists, etc
-eval $(/usr/bin/gnome-keyring-daemon --daemonize --login --start --components=gpg,pkcs11,secrets,ssh)
-export SSH_AUTH_SOCK
-export GPG_AGENT_INFO
-export GNOME_KEYRING_CONTROL
-export GNOME_KEYRING_PID
+if [[ -z $(pgrep -f gnome-keyring-daemon) ]];then
+    eval $(/usr/bin/gnome-keyring-daemon --daemonize --login --start --components=gpg,pkcs11,secrets,ssh)
+    export SSH_AUTH_SOCK
+    export GPG_AGENT_INFO
+    export GNOME_KEYRING_CONTROL
+    export GNOME_KEYRING_PID
+fi 
 
-exec ${WMSESSION}
+[[ ${1:-""} != norun ]] && exec ${WMSESSION}
