3a9fa4286fe3419c7486bdb02f55929b1829bc77
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-23 10:57:05 +0100

    refresh db if needed
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- polybar/scripts/arch-updates.sh
+++ polybar/scripts/arch-updates.sh
@@ -4,5 +4,6 @@
     kitty --title "Arch Updates" --class floater /bin/sh -c "yay;read"
     # kitty-ctrl jump -t "Arch Updates" /bin/sh -c "yay;read"
 }
+[[ -n $(command -v checkupdates) ]] && checkupdates || sudo pacman -Fy
 n=$(yay -Qu | wc -l)
 [[ ${n} -gt 0 ]] && echo "${n}"
