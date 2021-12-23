71a2a3fb0650c9f39fcbbea9840f07ddf86356c2
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-23 12:13:55 +0100

    add logging

--- i3/scripts/chmou_layouts.py
+++ i3/scripts/chmou_layouts.py
@@ -14,38 +14,57 @@ import click
 import i3ipc
 
 RESIZE_CLASSES = ["kitty", "emacs", "firefox", "chromium", "slack"]
+VERBOSE = False
+def log(msg):
+    if VERBOSE:
+        print(msg)
 
 def disable_floating(windows):
     [w.command("floating disable") for w in windows]
 
 @click.command()
-def gw():
+@click.option("--verbose", "-v", is_flag=True)
+def gw(verbose=False):
+    global VERBOSE
+    if verbose:
+        VERBOSE=True
+    
     i3 = i3ipc.Connection()
     current_workspace = i3.get_tree().find_focused().workspace()
     windows = [x for x in current_workspace if x.name]
+    log(f"Number of windows on screen {len(windows)}")
     screen_w = current_workspace.ipc_data['rect']['width']
+    log(f"Screen width {screen_w}")
     current_workspace.command("layout splith")
 
     if len(windows) == 2:
+        log("Using two windows mode")
         left_window = windows[0]
         left_window_width = round(screen_w / 3.5)
+        log(f"Left windows width: {left_window_width}")
         right_window_width = screen_w - left_window_width
+        log(f"Right windows width: {right_window_width}")
         disable_floating(windows)
         left_window.command(f"resize set {left_window_width}")
     elif len(windows) == 3:
+        log("Using three windows mode")
         left_window = windows[0]
         right_window = windows[2]
         left_window_width = round(screen_w / 5)
+        log(f"Left windows width: {left_window_width}")
         right_window_width = round(screen_w / 6)
+        log(f"Right windows width: {right_window_width}")
         disable_floating(windows)
         left_window.command(f"resize set {left_window_width}")
         right_window.command(f"resize set {right_window_width}")
     elif len(windows) == 1:
+        log("Using single window mode")
         window = windows[0]
-        windows_size = round(screen_w - (screen_w / 4))
+        window_size = round(screen_w - (screen_w / 4))
         window.command("floating enable")
         if window.window_class.lower() in RESIZE_CLASSES:
-            window.command(f"resize set width {windows_size}px")
+            log(f"Resize window to : {window_size}")
+            window.command(f"resize set width {window_size}px")
         window.command("move position center")
 
 if __name__ == '__main__':
