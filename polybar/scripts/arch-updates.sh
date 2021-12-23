532972b84c500f5a8f5aecdd8a1016183f429437
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-23 12:03:49 +0100

    run checkupdates before showing the arch package updates

--- polybar/scripts/arch-updates.sh
+++ polybar/scripts/arch-updates.sh
@@ -4,6 +4,6 @@
     kitty --title "Arch Updates" --class floater /bin/sh -c "yay;read"
     # kitty-ctrl jump -t "Arch Updates" /bin/sh -c "yay;read"
 }
-[[ -n $(command -v checkupdates) ]] && checkupdates || sudo pacman -Fy
+[[ -n $(command -v checkupdates) ]] && checkupdates >/dev/null || sudo pacman -Fy >/dev/null
 n=$(yay -Qu | wc -l)
 [[ ${n} -gt 0 ]] && echo "${n}"
