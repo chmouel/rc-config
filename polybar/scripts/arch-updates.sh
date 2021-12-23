6d48b7a99eb9a185e21802dbb53359727f1698aa
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-23 22:11:22 +0100

    fix checking updates

--- polybar/scripts/arch-updates.sh
+++ polybar/scripts/arch-updates.sh
@@ -4,6 +4,11 @@
     kitty --title "Arch Updates" --class floater /bin/sh -c "yay;read"
     # kitty-ctrl jump -t "Arch Updates" /bin/sh -c "yay;read"
 }
-[[ -n $(command -v checkupdates) ]] && checkupdates >/dev/null || sudo pacman -Fy >/dev/null
+if [[ -n $(command -v checkupdates) ]];then 
+    checkupdates 
+else
+    sudo pacman -Fy >/dev/null
+fi
+
 n=$(yay -Qu | wc -l)
 [[ ${n} -gt 0 ]] && echo "${n}"
