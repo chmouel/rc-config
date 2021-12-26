6358c24afe0d9c490b54daa053ab687d1a3df9f8
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-26 15:04:58 +0100

    improve move_to_biggest scrpt

--- i3/scripts/move_to_biggest.py
+++ i3/scripts/move_to_biggest.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 #
 # Promotes the focused window by swapping it with the largest window.
+#
 # adapted from https://aduros.com/blog/hacking-i3-window-promoting/
+#
+# It tries to do the right thing, to focus the current window in the largest area
+# unless -n is specified, may work best only for two windows, maybe we can add
+# rotation between windows but that would need some persistence.
 
 import click
 import i3ipc
@@ -16,7 +21,14 @@ def find_biggest_window(container):
         if not leaf.focused and area > max_area:
             max_area = area
             max_leaf = leaf
-    return max_leaf
+    return (max_leaf, max_area)
+
+
+def get_focused_area(container):
+    for leaf in container.leaves():
+        if leaf.focused:
+            rect = leaf.rect
+            return rect.width * rect.height
 
 
 @click.command()
@@ -26,10 +38,13 @@ def move_to_biggest(no_focus: bool = True):
     for reply in i3.get_workspaces():
         if reply.focused:
             workspace = i3.get_tree().find_by_id(reply.ipc_data["id"])
-            master = find_biggest_window(workspace)
+            focused_area = get_focused_area(workspace)
+            master, master_area = find_biggest_window(workspace)
             i3.command("swap container with con_id %s" % master.id)
+
             if not no_focus:
-                master.command("focus")
+                if focused_area >= master_area:
+                    master.command("focus")
 
 
 if __name__ == '__main__':
