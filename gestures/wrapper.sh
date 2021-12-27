d26b6a5c1d3e0a7830af0fcbdc01f38038c83b85
Chmouel Boudjnah <chmouel@chmouel.com>
2021-12-27 01:32:26 +0100

    use SWAYSOCK
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- gestures/wrapper.sh
+++ gestures/wrapper.sh
@@ -2,6 +2,6 @@
 set -eufx
 
 cfg=$HOME/.config/gestures/xorg.conf
-[[ -n ${GDMSESSION:-""} && ${GDMSESSION} == "sway" ]] && cfg=$HOME/.config/gestures/sway.conf
+[[ -n ${SWAYSOCK:-""} ]] && cfg=$HOME/.config/gestures/sway.conf
 
 libinput-gestures -c ${cfg}
