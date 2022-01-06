14832ceca753a897ee02fc872d37bb48f40ae0a5
Chmouel Boudjnah <chmouel@chmouel.com>
2022-01-06 09:38:29 +0100

    show noah's calendar too by default

--- polybar/scripts/clockclik.sh
+++ polybar/scripts/clockclik.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eux
 
-kitty --class=GClock -- /bin/bash -c "batz;echo;cal -3;gcalcli agenda --nodeclined --default-calendar=Work;read"
+kitty --class=GClock -- /bin/bash -c "batz;echo;cal -3;gcalcli agenda --nodeclined --default-calendar=Lyc --default-calendar=Work --details end --details=calendar --color-now-marker=green |head -15;read"
