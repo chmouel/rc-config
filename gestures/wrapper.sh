7db3a7e58614d83aae57d97ca304b54a122f03d5
Chmouel Boudjnah <chmouel@chmouel.com>
2022-02-04 08:02:56 +0100

    exit if GDMSESSION == wayland

--- gestures/wrapper.sh
+++ gestures/wrapper.sh
@@ -3,5 +3,6 @@ set -eufx
 
 cfg=$HOME/.config/gestures/xorg.conf
 [[ -n ${SWAYSOCK:-""} ]] && cfg=$HOME/.config/gestures/sway.conf
+[[ -n ${GDMSESSION:-""} && $GDMSESSION == gnome-wayland ]] && exit 0
 
 libinput-gestures -c ${cfg}
