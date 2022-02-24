2da0073b590396cf85c2ed1cb42fa62ca5d7d52b
Chmouel Boudjnah <chmouel@chmouel.com>
2022-02-24 11:21:33 +0100

    add meeting url

--- polybar/scripts/clockclik.sh
+++ polybar/scripts/clockclik.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eux
 
-kitty --class=GClock -- /bin/bash -c "batz;echo;cal -3;gcalcli agenda --nodeclined --default-calendar=Lyc --default-calendar=Work --details end --details=calendar --color-now-marker=green |head -15;read"
+kitty --class=GClock -- /bin/bash -c "batz;echo;cal -3;gcalcli agenda --nodeclined --default-calendar=Lyc --default-calendar=Work --details end --details=conference --details=calendar --color-now-marker=green |grep -Ev 'Conference Link: (more|sip|phone)'|head -15;read"
